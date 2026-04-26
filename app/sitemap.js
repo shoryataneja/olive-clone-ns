15a727a1df20880ffb0871d3ea24d394ddae335f
Shorya Taneja <shoryataneja5@gmail.com>
2026-04-26 06:46:08 +0530

    update sitemap with section URLs

--- app/sitemap.js
+++ app/sitemap.js
@@ -6,5 +6,11 @@ export default function sitemap() {
       changeFrequency: 'monthly',
       priority: 1,
     },
+    {
+      url: 'https://oliveapp.com/#how-it-works',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 0.8,
+    },
   ];
 }
